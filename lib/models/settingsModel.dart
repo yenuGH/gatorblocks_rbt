1cee1684e659c12384afc9e6fa252c6b57dc3833
reverbcs <jonkang02@outlook.com>
2020-05-10 12:37:28 -0700

    unbroke the icons on settings page

--- lib/models/settingsModel.dart
+++ lib/models/settingsModel.dart
@@ -1,49 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:gatorblocks_rbt/pages/classes.dart';
-
 
 class Option {
   Icon icon;
   String title;
   String subtitle;
 
-  Option(IconButton iconButton, {this.icon, this.title, this.subtitle});
+  Option({this.icon, this.title, this.subtitle});
 }
 
 final options = [
   Option(
-    IconButton(
-      onPressed: (){},
-      icon: Icon(Icons.settings, size: 40.0),
-    ),
+    icon: Icon(Icons.settings, size: 40.0),
     title: 'General',
     subtitle: 'Customize to make it your own.',
   ),
   Option(
-    IconButton(
-      onPressed: (){
-        print('Test');
-        BuildContext context;
-        Navigator.push(context, MaterialPageRoute(builder: (context) => Classes()));
-      },
-      icon: Icon(Icons.class_, size: 40.0),
-    ),
+    icon: Icon(Icons.class_, size: 40.0),
     title: 'Classes',
     subtitle: 'Manage your classes.',
   ),
   Option(
-    IconButton(
-      onPressed: (){},
-      icon: Icon(Icons.account_circle, size: 40.0),
-    ),
+    icon: Icon(Icons.account_circle, size: 40.0),
     title: 'Account',
     subtitle: 'Manage account options.',
   ),
   Option(
-    IconButton(
-      onPressed: (){},
-      icon: Icon(Icons.laptop, size: 40.0),
-    ),
+    icon: Icon(Icons.laptop, size: 40.0),
     title: 'Useless Button',
     subtitle: 'You\'re just wasting your time clicking this button. Stop. Stop it.',
   ),
